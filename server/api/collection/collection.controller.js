bb493b3b9bd8fe54ce229f9566406941dcc81dbb
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-10-06 15:38:06 -0700

    (feat) updated collections controller and model with new schema

--- server/api/collection/collection.controller.js
+++ server/api/collection/collection.controller.js
@@ -6,7 +6,9 @@ var Collection = require('./collection.model');
 // Get list of collections
 exports.index = function(req, res) {
   Collection.find(function (err, collections) {
-    if(err) { return handleError(res, err); }
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(200, collections);
   });
 };
@@ -14,8 +16,12 @@ exports.index = function(req, res) {
 // Get a single collection
 exports.show = function(req, res) {
   Collection.findById(req.params.id, function (err, collection) {
-    if(err) { return handleError(res, err); }
-    if(!collection) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!collection) {
+      return res.send(404);
+    }
     return res.json(collection);
   });
 };
@@ -23,20 +29,32 @@ exports.show = function(req, res) {
 // Creates a new collection in the DB.
 exports.create = function(req, res) {
   Collection.create(req.body, function(err, collection) {
-    if(err) { return handleError(res, err); }
+    console.log(req.body);
+    if (err) {
+      return handleError(res, err);
+    }
     return res.json(201, collection);
   });
 };
 
 // Updates an existing collection in the DB.
 exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
+  // Ensure the database id is not overwritten
+  if (req.body._id) {
+    delete req.body._id; 
+  }
   Collection.findById(req.params.id, function (err, collection) {
-    if (err) { return handleError(res, err); }
-    if(!collection) { return res.send(404); }
+    if (err) {
+      return handleError(res, err); 
+    }
+    if (!collection) {
+      return res.send(404); 
+    }
     var updated = _.merge(collection, req.body);
     updated.save(function (err) {
-      if (err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err); 
+      }
       return res.json(200, collection);
     });
   });
@@ -45,10 +63,16 @@ exports.update = function(req, res) {
 // Deletes a collection from the DB.
 exports.destroy = function(req, res) {
   Collection.findById(req.params.id, function (err, collection) {
-    if(err) { return handleError(res, err); }
-    if(!collection) { return res.send(404); }
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!collection) {
+      return res.send(404);
+    }
     collection.remove(function(err) {
-      if(err) { return handleError(res, err); }
+      if (err) {
+        return handleError(res, err);
+      }
       return res.send(204);
     });
   });
--- server/api/collection/collection.model.js
+++ server/api/collection/collection.model.js
@@ -4,9 +4,21 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var CollectionSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  title: String,
+  description: String,
+  // links contained in this collection
+  links: [
+    {
+      url: String,
+      description: String,
+    }
+  ],
+  // user_id for the collection's creating user
+  user: String,
+  // custom url for sharing this collection
+  url: String,
+  // Array containing user_ids of users who have starred this collection
+  starred_users: [String]
 });
 
 module.exports = mongoose.model('Collection', CollectionSchema);
