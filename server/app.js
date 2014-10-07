361ae001e96b5501bcec3d4036ce158326852a6c
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-10-07 15:04:11 -0700

    (doc) added comments

--- server/app.js
+++ server/app.js
@@ -15,7 +15,9 @@ var config = require('./config/environment');
 mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Populate DB with sample data
-if(config.seedDB) { require('./config/seed'); }
+if(config.seedDB) {
+  require('./config/seed');
+}
 
 // Setup server
 var app = express();
